49a4d1370502d72585bcbfdb2600e2473b36cb41
ZMH <2676708097@qq.com>
2025-05-13 01:20:38 +0800

    Modify the file path.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from thread import threaded_map_reduce
 
 if __name__ == "__main__":
-    filename = "AComp_Passenger_data_no_error.csv"
+    filename = "data/AComp_Passenger_data_no_error.csv"
     reduced = threaded_map_reduce(filename, num_threads=4)
 
     # Find top passengers with maximum flights
