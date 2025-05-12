7462ad63e354434dca98060e51ac65957ae28a5e
ZMH <2676708097@qq.com>
2025-05-13 01:17:25 +0800

    Compute the max number of flights each passenger has taken and print the results.

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from thread import threaded_map_reduce
 if __name__ == "__main__":
     filename = "AComp_Passenger_data_no_error.csv"
     reduced = threaded_map_reduce(filename, num_threads=4)
+
+    max_flights = max(reduced.values())
+    top_passengers = [pid for pid, count in reduced.items() if count == max_flights]
+
+    print("Passenger IDs (with the most flights):", top_passengers)
+    print("Number of flights:", max_flights)
