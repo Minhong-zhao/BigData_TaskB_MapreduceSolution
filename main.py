178461afeb994db2784dfd9cecf57cc8dc685ead
ZMH <2676708097@qq.com>
2025-05-13 12:23:51 +0800

    main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from thread import threaded_map_reduce
 
+
 if __name__ == "__main__":
     filename = "data/AComp_Passenger_data_no_error.csv"
     reduced = threaded_map_reduce(filename, num_threads=4)
